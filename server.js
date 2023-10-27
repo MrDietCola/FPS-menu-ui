31ed51997608f87d1e6ec193ecc9f6390029e5d5
AJ Seidler <ajseidler0526@gmail.com>
2023-10-27 09:29:16 -0500

    saved user data on front end

--- server.js
+++ server.js
@@ -1,31 +1,24 @@
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
+const api = require('./routes/index'); 
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/api', api);
 
+// sets static page to index 
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
+// route for notes
 app.get('/multiplayer', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/multiplayer.html'))
+  res.sendFile(path.join(__dirname, '/public/multiplayer.html'))
 );
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/multiplayer.html'))
-);
-
+// listening for server
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
 );
